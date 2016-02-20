f63f814ca69edf6f76afbde27d15fe5cc1c4a3d6
Guilherme Leobas <guilhermeleobas@gmail.com>
2016-02-20 01:35:53 +0000

    change output format

--- test/testFormat.js
+++ test/testFormat.js
@@ -4,29 +4,27 @@ var chai = require ('chai');
 var expect = require ('chai').expect;
 var format = require ('./../src/format.js');
 
-var indent = '      ';
-
-describe ('test format', function (){
+describe ('test formatLines method', function (){
 
   it ('should format an output with multiple lines', function (){
     let input = "6\n5\n4\n3\n2\n3\n3\n1\n1\n3\n2\n2\n1\n2\n3\n";
 
     let eOutput = "" +
-      "      6\n" + 
-      "      5\n" + 
-      "      4\n" + 
-      "      3\n" + 
-      "      2\n" + 
-      "      3\n" + 
-      "      3\n" + 
-      "      1\n" + 
-      "      1\n" + 
-      "      3\n" + 
-      "      2\n" + 
-      "      2\n" + 
-      "      1\n" + 
-      "      2\n" + 
-      "      3";
+      "6\n" + 
+      "5\n" + 
+      "4\n" + 
+      "3\n" + 
+      "2\n" + 
+      "3\n" + 
+      "3\n" + 
+      "1\n" + 
+      "1\n" + 
+      "3\n" + 
+      "2\n" + 
+      "2\n" + 
+      "1\n" + 
+      "2\n" + 
+      "3\n";
 
 
     let output = format.formatLines (input);
@@ -37,7 +35,7 @@ describe ('test format', function (){
   it ('should format an inline output with only one number', function (){
 
     let input = "6\n";
-    let expectedOutput = "      6\n";
+    let expectedOutput = "6\n";
 
     let output = format.formatLines (input);
 
@@ -47,7 +45,7 @@ describe ('test format', function (){
   it ('should format an inline output with multiple letters', function (){
 
     let input = "6 5 4 1 3";
-    let expectedOutput = "      6 5 4 1 3\n";
+    let expectedOutput = "6 5 4 1 3\n";
 
     let output = format.formatLines (input);
 
@@ -59,21 +57,21 @@ describe ('test format', function (){
     let input = "6 6 6\n5 5 5\n4 4 4\n3 3 3\n2 2 2\n3 3 3\n3 3 3\n1 1 1\n1 1 1\n3 3 3\n2 2 2\n2 2 2\n1 1 1\n2 2 2\n3 3 3\n";
 
     let expectedOutput = "" +
-      "      6 6 6\n" + 
-      "      5 5 5\n" + 
-      "      4 4 4\n" + 
-      "      3 3 3\n" + 
-      "      2 2 2\n" + 
-      "      3 3 3\n" + 
-      "      3 3 3\n" + 
-      "      1 1 1\n" + 
-      "      1 1 1\n" + 
-      "      3 3 3\n" + 
-      "      2 2 2\n" + 
-      "      2 2 2\n" + 
-      "      1 1 1\n" + 
-      "      2 2 2\n" + 
-      "      3 3 3";
+      "6 6 6\n" + 
+      "5 5 5\n" + 
+      "4 4 4\n" + 
+      "3 3 3\n" + 
+      "2 2 2\n" + 
+      "3 3 3\n" + 
+      "3 3 3\n" + 
+      "1 1 1\n" + 
+      "1 1 1\n" + 
+      "3 3 3\n" + 
+      "2 2 2\n" + 
+      "2 2 2\n" + 
+      "1 1 1\n" + 
+      "2 2 2\n" + 
+      "3 3 3\n";
 
     let output = format.formatLines (input);
 
@@ -83,9 +81,9 @@ describe ('test format', function (){
   it ('should format output [\\n<\\n{\\n', function (){
     let input = "[\n<\n{\n";
     let eoutput = "" +
-      "      [\n" +
-      "      <\n" +
-      "      {";
+      "[\n" +
+      "<\n" +
+      "{\n";
       
     let output = format.formatLines (input);
     
@@ -95,12 +93,50 @@ describe ('test format', function (){
   it ('should format output {\\n(\\n[\\n', function (){
     let input = "{\n(\n[\n";
     let eoutput = "" + 
-      indent + "{\n" +
-      indent + "(\n" +
-      indent + "[";
+      "{\n" +
+      "(\n" +
+      "[\n";
       
     expect (format.formatLines (input)).to.equals (eoutput);
-  })
-
+  });
 
 });
+
+
+
+describe ('test formatOutput method', function (){
+
+  it ('Test  a simple input', function (){
+
+    let input = {
+      'stdout': "{\n(\n[\n",
+      'err': 'ok',
+      'time': '33ms'
+    }
+    let output = "" + 
+      "{\n" +
+      "(\n" +
+      "[\n";
+
+    let index = 0;
+
+    let diffStatus = false;
+
+    let eoutput = '' +
+      '## Test #0' +
+      '\n' +
+      'Wrong answer - 33ms' + '\n' + 
+      '+ expected  - actual' + '\n' +
+      '-~-' + '\n' +
+      '{\n' +
+      '(\n' +
+      '[\n' +
+      '-~-' + '\n' +
+      '{\n' +
+      '(\n' +
+      '[\n' +
+      '-~-' + '\n';
+    
+    expect (format.formatOutput (input, output, index, diffStatus, false)).to.equals (eoutput);
+  })
+})
