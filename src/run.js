84f25e9fa86664f8b965cfe924c37ae742515939
charlieamer <amer_zavlan@live.com>
2016-05-05 00:25:50 +0200

    fixed bug where loading strings in node would have trailing newline

--- src/run.js
+++ src/run.js
@@ -9,7 +9,7 @@ var runCommand = {
   "c": "./main",
   "c++": "./main",
   "python": "python %s",
-  "node": "node -e \"readline=function(){var n=require('fs');require('readline');return win32=function(){return'win32'===process.platform},readSync=function(r){var e=win32()?process.stdin.fd:n.openSync('/dev/stdin','rs'),t=n.readSync(e,r,0,r.length);return win32()||n.closeSync(e),t},function(n){try{return n.toString(null,0,readSync(n))}catch(r){throw r}}(new Buffer(256))};print=console.log;console.log=function(){};write=process.stdout.write;require('./%s')\"",
+  "node": "node -e \"readline=function(){var n=require('fs');require('readline');return win32=function(){return'win32'===process.platform},readSync=function(r){var e=win32()?process.stdin.fd:n.openSync('/dev/stdin','rs'),t=n.readSync(e,r,0,r.length);return win32()||n.closeSync(e),t},function(n){return n.toString(null,0,readSync(n)).replace('\\n','').replace('\\r','')}(new Buffer(256))};print=console.log;console.log=function(){};write=process.stdout.write;require('./%s')\"",
   "python3": "python3 %s"
 };
 
